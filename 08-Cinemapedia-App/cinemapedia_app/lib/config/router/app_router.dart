c6b2f855e3867f6d3137ea4478947885b2b306f1
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2023-11-05 17:31:50 +0100

    ShellRoutes

--- 08-Cinemapedia-App/cinemapedia_app/lib/config/router/app_router.dart
+++ 08-Cinemapedia-App/cinemapedia_app/lib/config/router/app_router.dart
@@ -1,26 +1,63 @@
+// import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cinemapedia_app/presentation/screens/screens.dart';
+import 'package:cinemapedia_app/presentation/views/views.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
 
-            return MovieScreen(
-              movieId: movieId,
-            );
+                return MovieScreen(
+                  movieId: movieId,
+                );
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
           },
         ),
       ],
     ),
+
+    //   GoRoute(
+    //     path: '/',
+    //     name: HomeScreen.name,
+    //     builder: (context, state) => const HomeScreen(
+    //       childView: HomeView(),
+    //     ),
+    //     routes: [
+    //       GoRoute(
+    //         path: 'movie/:id',
+    //         name: MovieScreen.name,
+    //         builder: (context, state) {
+    //           final movieId = state.pathParameters['id'] ?? 'no-id';
+
+    //           return MovieScreen(
+    //             movieId: movieId,
+    //           );
+    //         },
+    //       ),
+    //     ],
+    //   ),
   ],
 );
--- 08-Cinemapedia-App/cinemapedia_app/lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ 08-Cinemapedia-App/cinemapedia_app/lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,23 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/categories');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home_max),
-        label: 'Inicio',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.label_outline),
-        label: 'Categorías',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.favorite_outline),
-        label: 'Favoritos',
-      ),
-    ]);
+    return BottomNavigationBar(
+      elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (value) => _onItemTapped(context, value),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_max),
+          label: 'Inicio',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.label_outline),
+          label: 'Categorías',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Favoritos',
+        ),
+      ],
+    );
   }
 }
